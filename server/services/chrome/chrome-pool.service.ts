8bf895a53ff3e3ad6157b82e27fe404f26114cd8
Brody Smith <smithbr4@gmail.com>
2017-06-04 14:35:17 -0600

    chrome-pool can handle headless and non-headless

--- server/services/chrome/chrome-pool.service.ts
+++ server/services/chrome/chrome-pool.service.ts
@@ -12,32 +12,43 @@ export class ChromePoolService {
     instance based on how many are still open. Also many this should explore browser context?
      */
     async getChromeInstance(url:string){
-        let targetClient = await CDP({tab: 'ws://localhost:9222/devtools/browser'})
-        console.log('new client created.')
-        let {Target} = targetClient
-        let browserContextResponse = await Target.createBrowserContext({})
-        console.log('Created Browser Context' + JSON.stringify(browserContextResponse))
-        let targetResponse = await Target.createTarget({
-            url: url,
-            width: 1280,
-            height: 1096,
-            browserContextId: browserContextResponse.browserContextId
-        })
-        console.log('Target Response' + JSON.stringify(targetResponse))
+        let client: any
+        let target: any
+        if(this.configService.config.chrome.headless){
+            let targetClient = await CDP({tab: 'ws://localhost:9222/devtools/browser'})
+            console.log('new client created.')
+            let {Target} = targetClient
+            let browserContextResponse = await Target.createBrowserContext({})
+            console.log('Created Browser Context' + JSON.stringify(browserContextResponse))
+            let targetResponse = await Target.createTarget({
+                url: url,
+                width: 1280,
+                height: 1096,
+                browserContextId: browserContextResponse.browserContextId
+            })
+            console.log('Target Response' + JSON.stringify(targetResponse))
 
-        let targets = await CDP.List()
-        let target = targets.filter((target:any) => {
-            return target.id === targetResponse.targetId
-        })[0]
-        console.log('Found Target: ' + JSON.stringify(target))
-        let client = await CDP({
+            let targets = await CDP.List()
+            target = targets.filter((target:any) => {
+                return target.id === targetResponse.targetId
+            })[0]
+            console.log('Found Target: ' + JSON.stringify(target))
+        }else{
+            target = await CDP.New({
+                url: url
+            })
+            console.log('Created Target')
+        }
+
+        client = await CDP({
             target: target
         })
+
         await client.Page.enable()
         await client.Runtime.enable()
         await client.Network.enable()
 
         console.log('Page Enabled')
-        return new Chrome(targetResponse.targetId, client, CDP, this.configService.config.dataDir)
+        return new Chrome(target.id, client, CDP, this.configService.config.dataDir)
     }
 }
